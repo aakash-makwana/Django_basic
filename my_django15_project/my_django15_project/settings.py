8efd836244c9afe3431d63a3b64756fdb5602c58
aakash-makwana <amprinceaki11@gmail.com>
2016-06-02 20:44:45 +0530

    Django admin setup

--- my_django15_project/my_django15_project/settings.py
+++ my_django15_project/my_django15_project/settings.py
@@ -9,15 +9,24 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+#         'NAME': '',                      # Or path to database file if using sqlite3.
+#         # The following settings are not used with sqlite3:
+#         'USER': '',
+#         'PASSWORD': '',
+#         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+#         'PORT': '',                      # Set to empty string for default.
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'SelfDev',
+        'USER': 'root',
+        'PASSWORD': 'root',
     }
 }
 
@@ -113,6 +122,18 @@ TEMPLATE_DIRS = (
     # Don't forget to use absolute paths, not relative paths.
 )
 
+# INSTALLED_APPS = (
+#     'django.contrib.auth',
+#     'django.contrib.contenttypes',
+#     'django.contrib.sessions',
+#     'django.contrib.sites',
+#     'django.contrib.messages',
+#     'django.contrib.staticfiles',
+#     # Uncomment the next line to enable the admin:
+#     # 'django.contrib.admin',
+#     # Uncomment the next line to enable admin documentation:
+#     # 'django.contrib.admindocs',
+# )
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -120,10 +141,9 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admin',
+    'myapp',
+    'south',
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
--- readme.txt
+++ readme.txt
@@ -5,3 +5,12 @@ go to your directory where you want to create project
 $ virtualenv env
 $ source env/bin/activate
 $ deactivate
+
+for Mysql error
+http://www.techstricks.com/mysql_config-not-found-mysql-python-on-mac-osx/
+$ export PATH=$PATH:/usr/local/mysql/bin
+$ cd my_django15_projectpip install mysql-python --no-cache-dir
+
+Username : root
+Pwd : root
+Email : amprinceaki11@gmail.com
